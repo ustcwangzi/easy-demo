527300f2aaa75bf0cb27a155420e25bee61b3d6c
wangzi <ustcwangzi@foxmail.com>
2020-11-21 15:29:58 +0800

    init

---
+++ src/main/java/com/wz/easydemo/EasyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wz.easydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDemoApplication.class, args);
+    }
+
+}
